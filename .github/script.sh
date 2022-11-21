4a42b88e0d289bc469e1cfc072255a43c7501d88
devopsadvocate007 <93682230+devopsadvocate007@users.noreply.github.com>
2022-11-20 20:24:22 -0500

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Validate not sudo
+user_id=`id -u`
+if [ $user_id -eq 0 -a -z "$RUNNER_ALLOW_RUNASROOT" ]; then
+    echo "Must not run interactively with sudo"
+    exit 1
+fi
 
-sudo su -
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 app_name=$1
 image=$2
 namespace=$3
